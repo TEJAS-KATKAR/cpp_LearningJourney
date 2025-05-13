45d0c37983f62db941f550ad348eb2d22bfdb5c6
Tejas_1016 <tkatkar1256@gmail.com>
2025-05-13 20:52:21 +0530

    deleted

--- DSA_in_c++/4-Loops/3-For_loop.cpp
+++
@@ -1,9 +0,0 @@
-//-----------------------LOOPS(for loop)--------------------------
-#include<iostream>
-int main(){
-  for(int i = 1; i <= 3; i++){
-    std::cout <<"HAPPY NEW YEAR!\n";
-  }
-  
-  return 0;
-}
